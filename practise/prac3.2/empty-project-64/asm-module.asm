32c959ebe2e5251a07b6858ce4ff9bc25e9d5d3d
Jan Zamostny <jan.zamostny04@gmail.com>
2025-03-09 17:10:47 +0100

    practice 3

---
+++ practise/prac3.2/empty-project-64/asm-module.asm
@@ -0,0 +1,29 @@
+    bits 64
+
+    section .bss
+
+
+;***************************************************************************
+
+    section .text
+        extern bubble
+        extern strup
+        extern nejcastejsi
+        extern prvocislo
+
+bubble:
+
+    ret
+
+strup:
+
+    ret
+
+nejcastejsi:
+
+    ret
+
+prvocislo:
+
+    ret
+
---
+++ practise/prac3.2/empty-project-64/c-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void bubble( int *tp_int, int t_N );
+void strup( char *t_str );
+char nejcastejsi( char *t_str );
+long prvocislo( long t_cislo );
+
+int main()
+{
+  int pole[] = {1,5,6,0,10,-11,4,6,2,9};
+  char str[] = "Ahoj, jak se mas?";
+  unsigned int cislo = 10;
+
+  return 0;
+}
